09882652d1dffbc799b23ce2d1d1698f57ac38be
xingchen-fan <xf82@cornell.edu>
2024-09-06 17:35:17 +0200

    submitter

---
+++ CRAB_script/submitter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+config=$1
+nsub=$2
+logname=$3
+outdir=$4
+tag=$5
+
+count=1
+while [ $count -le $((nsub)) ]
+do
+    sed -i "s@\(config.General.requestName = \).*@\1\'$logname$count\'@g" $config
+    sed -i "s@\(config.JobType.scriptArgs = \).*@\1[\'Nevents=10000\', \'Tag=$tag$count\', \'DIR=$outdir\']@g" $config
+    crab submit -c $config
+    echo Submission $count is submitted.
+    ((count++))
+done
+echo All submitted.
